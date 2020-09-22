2011d763d017ba92d89bf62e7f23d5df8e707617
pelayo-torre <pelayogtorre@gmail.com>
2020-09-22 21:21:58 +0200

    Se crea un mapa con los marcadores de cada estación en España

--- mapa.js
+++ mapa.js
@@ -1,16 +1,25 @@
 //Representa el mapa geográfico
 var map;
 
+//Capa de KML
+var ctaLayer;
+
 /**
  * Inicialización del mapa
  */
 function initMap() {
     var misOpciones = {
         center: {lat: 43.354810, lng: -5.851805},
-        zoom: 9,
+        zoom: 7,
         mapTypeId: google.maps.MapTypeId.SATELLITE,
-        mapTypeControl: false,
-        streetViewControl: false
     };
     map = new google.maps.Map(document.getElementById("map_frame"), misOpciones);
+
+    ctaLayer = new google.maps.KmlLayer({
+        url: 'https://www.dropbox.com/s/c76evcnvm65bizb/EstacionesCA_2016.kml?dl=1',
+        map: map
+    });
+
+    console.log(ctaLayer)
+
 }
